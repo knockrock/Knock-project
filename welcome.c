3085ffea5a24b089c05f054c645b817f15add61d
Pinokcio <mryu12@naver.com>
2017-12-08 20:20:12 +0900

    Delete welcome.c

--- welcome.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "movie_manage_program.h"
-
-void welcome() // 프로그램 시작
-{
-	printf(">> Welcome to My Movie <<\n");
-	printf("File Loading.....\n");
-	printf("You can use add, update, delete, search, sort, save, end commands.\n");
-}
